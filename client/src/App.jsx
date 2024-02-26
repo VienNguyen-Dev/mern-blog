11937b6da5ccb827030a515e69376181b14707be
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-02-26 15:53:35 +0700

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Hello</>;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
