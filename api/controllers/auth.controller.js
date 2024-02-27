1b7dd54525e22b986562e0b2b80184ccae78a1a8
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-02-27 13:42:41 +0700

    Create Sign up page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   //Check username, email and password 
   if (!username || !email || !password || username === '' || email === '' || password === '') {
-    return next(errorHandler(400, "All filed are required"))
+    next(errorHandler(400, "All filed are required"))
   }
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = await User.create({
