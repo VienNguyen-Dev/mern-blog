c57fbdab283997c95b30d34dcdf690e1f09ae721
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-03-04 18:02:40 +0700

    Add create a post API route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from '../api/routes/user.route.js';
-import authRoutes from '../api/routes/auth.route.js'
+import authRoutes from '../api/routes/auth.route.js';
+import postRoutes from '../api/routes/post.route.js'
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -19,6 +20,7 @@ mongoose.connect(process.env.MONGO)
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
