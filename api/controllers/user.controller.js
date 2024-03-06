4c782987eabcfbff8eadab5b3f5f9c25c9736e15
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-03-06 23:02:39 +0700

    Create get user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -76,3 +76,47 @@ export const signout = async (req, res, next) => {
     next(error);
   }
 }
+
+export const getusers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allow to see all users"));
+  }
+
+  try {
+    const startIntex = parseInt(req.query.startIntex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+
+    const users = await User.find()
+      .sort({ createAt: sortDirection })
+      .skip(startIntex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password: pass, ...rest } = user._doc;
+      return rest;
+    })
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    )
+
+    const lastMonthUsers = await User.countDocuments(
+      { createdAt: { $gte: oneMonthAgo } }
+    )
+
+    res.status(200).json({
+      users,
+      totalUsers,
+      lastMonthUsers
+    })
+  } catch (error) {
+    next(error);
+  }
+}
